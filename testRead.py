673a63319822db818fe394d975a85f3d7fdd3602
nhung234 <nhung234.t@gmail.com>
2014-05-14 20:50:49 +0700

    Update testRead.py

--- testRead.py
+++ testRead.py
@@ -25,7 +25,7 @@ while True :
 	line_maf = serialIO.readline().split(" ")
 	maf = int("0x"+line_maf[4]+line_maf[5], 16)/100
 
-	f.write(repr(speed)+ "km/h,  " +repr(rpm)+ "rpm,  " +repr(maf)+ " grams/sec "+ )
+	f.write(repr(strftime("%H:%M:%S"))+"----"+repr(speed)+ "km/h,  " +repr(rpm)+ "rpm,  " +repr(maf)+ " grams/sec ")
 	print speed, "km/h ; ",rpm, "rpm ; ",maf, " grams/sec ; "
 	print "-------------------------------"
 
