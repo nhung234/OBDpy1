966d898b506a0ae4d442f3a2c7d2078c611e5918
nhung234 <nhung234.t@gmail.com>
2014-05-30 20:25:11 +0700

    Update testRead.py

--- testRead.py
+++ testRead.py
@@ -22,21 +22,21 @@ f = open(repr()"/"+strftime("%d-%m-%Y_%H:%M:%S")+".txt", "w")
 while True :
 	#print "------ "+strftime("%d-%m-%Y %H:%M:%S")+" ------"
 
-	serialIO.write("01 0C \r")
-	line_rpm = serialIO.readline().split(" ")
-	rpm = int("0x"+line_rpm[4]+line_rpm[5], 16)/4
+	#serialIO.write("01 0C \r")
+	#line_rpm = serialIO.readline().split(" ")
+	rpm = 1#int("0x"+line_rpm[4]+line_rpm[5], 16)/4
 
-	serialIO.write("01 0D \r")
-	line_speed = serialIO.readline().split(" ")
-	speed = int("0x"+line_speed[4], 16)
+	#serialIO.write("01 0D \r")
+	#line_speed = serialIO.readline().split(" ")
+	speed = 2#int("0x"+line_speed[4], 16)
 
 	#serialIO.write("01 04 \r")
 	#line_load = serialIO.readline().split(" ")
 	#load = int("0x"+line_load[4], 16)*100/255
 	
-	serialIO.write("01 10 \r")
-	line_maf = serialIO.readline().split(" ")
-	maf = int("0x"+line_maf[4]+line_maf[5], 16)/100
+	#serialIO.write("01 10 \r")
+	#line_maf = serialIO.readline().split(" ")
+	maf = 3#int("0x"+line_maf[4]+line_maf[5], 16)/100
 
 	f.write(repr(strftime("%H:%M:%S"))+"----"+repr(speed)+ "km/h,  " +repr(rpm)+ "rpm,  " +repr(maf)+ " grams/sec ")
 	#MPG calculation
